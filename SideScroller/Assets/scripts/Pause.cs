9952e1b566491035b100bb7339ae6fa46fb55463
Sarah <101369672+alpinealpaca@users.noreply.github.com>
2022-11-04 10:00:30 +1300

    Pause
    button colours and pausing

---
+++ SideScroller/Assets/scripts/Pause.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseScreen;
+    private bool Paused = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseScreen.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            if (Paused == false)
+            {
+                Time.timeScale = 0f;
+                PauseScreen.gameObject.SetActive(true);
+                Paused = true;
+                
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        PauseScreen.gameObject.SetActive(false);
+        Paused = false;
+    }
+
+    public void ExitButton()
+    {
+        SceneManager.LoadScene("TitleScreen");
+        Time.timeScale = 1f;
+    }
+}
